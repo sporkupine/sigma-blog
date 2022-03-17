f1a957b8bf397631508a947c9eda4083e94209f9
sporkupine <coffeechief25@gmail.com>
2022-03-16 23:39:12 -0500

    handle new/edit form with partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article saved!"
       redirect_to @article
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article updated!"
       redirect_to @article
     else
@@ -45,6 +45,11 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 
 
 end
