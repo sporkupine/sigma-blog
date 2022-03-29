f2e40ee6ccb71b2acabbdf97bd1f5d3d87626b55
sporkupine <coffeechief25@gmail.com>
2022-03-28 20:09:37 -0500

    add secure passwords to Users, update gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }, 
     length: { maximum: 105 }, 
     format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_003114) do
+ActiveRecord::Schema.define(version: 2022_03_29_010333) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_003114) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
