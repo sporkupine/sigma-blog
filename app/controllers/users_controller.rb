29d675abf309b7b28720b486f87160f3c35fda79
sporkupine <coffeechief25@gmail.com>
2022-03-31 19:28:25 -0500

    can now add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Thank you for signing up, #{@user.username}!"
+      # redirect_to @user
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
