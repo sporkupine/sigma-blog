69a7c959ca47ee126cbdb708a08430f581244e04
sporkupine <coffeechief25@gmail.com>
2022-03-31 19:41:46 -0500

    basic user editing enabled from edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account successfully updated."
+      #redirect_to @user
+    else
+      
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,8 +27,8 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-
   end
+
 
   private
 
