4a826838368b23fa2434f0b643c18b173898c1d9
sporkupine <coffeechief25@gmail.com>
2022-04-02 20:02:19 -0500

    add user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
+
   def new
     @user = User.new
   end
@@ -18,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account successfully updated."
-      #redirect_to @user
+      redirect_to @user
     else
       
     end
@@ -28,7 +33,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Thank you for signing up, #{@user.username}!"
-      # redirect_to @user
+      redirect_to @user
     else
       render 'new'
     end
