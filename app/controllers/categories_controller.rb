ab9102d34aa8aa246339e546573d42e72b92fb42
sporkupine <coffeechief25@gmail.com>
2022-04-07 01:36:40 -0500

    create category new&create views tests & controls

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,16 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "New category successfully created!"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -8,6 +18,14 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    
+
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+
 end
